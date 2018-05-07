7a593da45c8712f1e3f5c02879d7a2dfd2c3b07a
manoj-deepRanalytics <manoj.kumar@deepranalytics.com>
2018-05-07 08:18:11 -0400

    Comment unit test

--- main.cpp
+++ main.cpp
@@ -2,12 +2,13 @@
 #include <QApplication>
 #include <Windows.h>
 #include <gtest/gtest.h>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    ::testing::InitGoogleTest(&argc, argv);
-
-    return RUN_ALL_TESTS();
+    qDebug() <<" Running Test ";
+//    ::testing::InitGoogleTest(&argc, argv);
+//    return RUN_ALL_TESTS();
     return a.exec();
 }
