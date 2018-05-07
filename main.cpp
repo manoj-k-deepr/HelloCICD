6955d41b8a799ddb1f4e876f4b7111ca8dfaf674
manoj-deepRanalytics <manoj.kumar@deepranalytics.com>
2018-05-07 10:59:48 -0400

    Add gtest working.

--- main.cpp
+++ main.cpp
@@ -19,17 +19,7 @@ TEST(SquareRootTest, PositiveNos) {
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    std::cout <<" Running Test 1\n";
-    std::cout <<" Running Test 2\n";
-    std::cout <<" Running Test 3\n";
-    std::cout <<" Running Test 4\n";
-    std::cout <<" Running Test 5\n";
-    std::cout <<" Running Test 6\n";
-    std::cout <<" Running Test Failed\n";
-
-
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
-    exit(0);
     return a.exec();
 }
