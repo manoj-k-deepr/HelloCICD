bc7f7e42dbf3f8ea5524f6eb64497da85eeef260
manoj-deepRanalytics <manoj.kumar@deepranalytics.com>
2018-05-07 10:37:06 -0400

    stop exe

--- main.cpp
+++ main.cpp
@@ -3,11 +3,12 @@
 #include <Windows.h>
 #include <gtest/gtest.h>
 #include <QDebug>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    qDebug() <<" Running Test ";
+    std::cout <<" Running Test ";
 //    ::testing::InitGoogleTest(&argc, argv);
 //    return RUN_ALL_TESTS();
     exit(0);
