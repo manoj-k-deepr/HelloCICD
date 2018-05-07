1ba6c5c479cac2d42912b6220eeef26fc54496c3
manoj-deepRanalytics <manoj.kumar@deepranalytics.com>
2018-05-07 10:48:27 -0400

    Display output

--- main.cpp
+++ main.cpp
@@ -8,7 +8,15 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    std::cout <<" Running Test ";
+    std::cout <<" Running Test 1";
+    std::cout <<" Running Test 2";
+    std::cout <<" Running Test 3";
+    std::cout <<" Running Test 4";
+    std::cout <<" Running Test 5";
+    std::cout <<" Running Test 6";
+    std::cout <<" Running Test Failed";
+
+
 //    ::testing::InitGoogleTest(&argc, argv);
 //    return RUN_ALL_TESTS();
     exit(0);
