c302d5b6720e33b19fa920c2afea9e213499c09e
manoj-deepRanalytics <manoj.kumar@deepranalytics.com>
2020-06-24 12:02:57 -0400

    change one

--- others/buzz_number.cpp
+++ others/buzz_number.cpp
@@ -18,3 +18,15 @@ int main() {
     }
     return 0;
 }
+//int n, t;
+//std::cin >> t;
+//while (t--) {
+//    std::cin >> n;
+//    if ((n % 7 == 0) || (n % 10 == 7))
+//        std::cout << n << " is a buzz number" << std::endl;
+//    else
+//        std::cout << n << " is not a buzz number" << std::endl;
+//}
+//return 0;
+
+
