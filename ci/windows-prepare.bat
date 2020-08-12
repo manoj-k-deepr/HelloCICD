3cbff7fc77e7b011de8e1320b1af1a453091c0c5
Manoj Kumar <manoj.kumar@deepranalytics.com>
2020-08-12 12:56:55 +0000

    Update ci/windows-prepare.bat

--- ci/windows-prepare.bat
+++ ci/windows-prepare.bat
@@ -4,3 +4,5 @@ set /p Build=<version.txt
 ECHO %Build%
 ECHO ".........................................................."
 
+apt-get update && \
+	apt-get install -y build-essential git cmake autoconf libtool pkg-config
