8c88cbb87fda5773c030071d923a7661c9dd442e
Manoj Kumar <manoj.kumar@deepranalytics.com>
2020-08-12 13:12:59 +0000

    Update CMakeLists.txt, ci/docker-prepare.sh, .gitlab-ci.yml files

--- ci/docker-prepare.sh
+++ ci/docker-prepare.sh
@@ -3,3 +3,12 @@
 
 apt-get update && \
 	apt-get install -y build-essential git cmake autoconf libtool pkg-config
+
+wget https://github.com/Kitware/CMake/releases/download/v3.15.2/cmake-3.15.2.tar.gz
+tar -zxvf cmake-3.15.2.tar.gz
+cd cmake-3.15.2
+
+#Run the following commands to compile and install CMake
+./bootstrap
+make
+make install
