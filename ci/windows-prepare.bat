0031959cd8936a487ceb47d8835d78fc6f855daa
Manoj Kumar <manoj.kumar@deepranalytics.com>
2020-08-12 12:58:26 +0000

    Update ci/windows-prepare.bat, ci/docker-prepare.sh, .gitlab-ci.yml files

--- ci/windows-prepare.bat
+++ ci/windows-prepare.bat
@@ -4,5 +4,4 @@ set /p Build=<version.txt
 ECHO %Build%
 ECHO ".........................................................."
 
-apt-get update && \
-	apt-get install -y build-essential git cmake autoconf libtool pkg-config
+
